8e639028fab7975aa83eaca7fd8162fda7fd62b8
Victor Luís <victorluiscaguiar@gmail.com>
2024-09-13 02:16:24 -0300

    Added database migration

--- Makefile
+++ Makefile
@@ -6,3 +6,12 @@ run: build
 
 test:
 	@go test -v ./...
+
+migration:
+	@migrate create -ext sql -dir cmd/migrate/migrations $(filter-out $@,$(MAKECMDGOALS))
+
+migrate-up:
+	@go run cmd/migrate/main.go up
+
+migrate-down:
+	@go run cmd/migrate/main.go down
--- cmd/main.go
+++ cmd/main.go
@@ -11,7 +11,7 @@ import (
 )
 
 func main() {
-	database, err := db.NewMySQLStorage(mysql.Config{
+	db, err := db.NewMySQLStorage(mysql.Config{
 		User:                 config.Envs.DBUser,
 		Passwd:               config.Envs.DBPassword,
 		Addr:                 config.Envs.DBAddress,
@@ -24,9 +24,9 @@ func main() {
 		log.Fatal(err)
 	}
 
-	initStorage(database)
+	initStorage(db)
 
-	server := api.NewAPIServer(":8080", database)
+	server := api.NewAPIServer(":8080", db)
 	if err := server.Run(); err != nil {
 		log.Fatalf("Failed to start the server: %v", err)
 	}
